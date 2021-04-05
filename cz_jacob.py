1b729b3f26d82250317617162e8ddb22173ab405
Daniil <dadyarri@gmail.com>
2021-04-06 00:45:39 +0300

    feat: inherit default config

--- cz_jacob.py
+++ cz_jacob.py
@@ -1,56 +1,167 @@
+import os
+import re
+from typing import List, Dict, Any
+
 from commitizen.cz.base import BaseCommitizen
 
 
 class JacobCz(BaseCommitizen):
     def questions(self) -> list:
         """Questions regarding the commit message."""
-        questions = [
+        questions: List[Dict[str, Any]] = [
             {
                 "type": "list",
-                "name": "type",
-                "message": "Выберите тип вносимых изменений",
+                "name": "prefix",
+                "message": "Select the type of change you are committing",
                 "choices": [
                     {
-                        "key": "fix",
-                        "value": "Исправление бага. PATCH в нотации SemVer",
+                        "value": "fix",
+                        "name": "fix: A bug fix. Correlates with PATCH in SemVer",
                     },
                     {
-                        "key": "docs",
-                        "value": "Изменение документации",
+                        "value": "feat",
+                        "name": "feat: A new feature. Correlates with MINOR in SemVer",
+                    },
+                    {"value": "docs", "name": "docs: Documentation only changes"},
+                    {
+                        "value": "style",
+                        "name": (
+                            "style: Changes that do not affect the "
+                            "meaning of the code (white-space, formatting,"
+                            " missing semi-colons, etc)"
+                        ),
                     },
                     {
-                        "key": "style",
-                        "value": "Изменения, не меняющие смысл кода (форматирование, пробелы, запятые, пр.)",
+                        "value": "refactor",
+                        "name": (
+                            "refactor: A code change that neither fixes "
+                            "a bug nor adds a feature"
+                        ),
                     },
                     {
-                        "key": "refactor",
-                        "value": "Изменения кода, ни добавляющие нового функционала, ни чинящие баги",
+                        "value": "perf",
+                        "name": "perf: A code change that improves performance",
                     },
                     {
-                        "key": "perf",
-                        "value": "Улучшения производительности",
+                        "value": "test",
+                        "name": (
+                            "test: Adding missing or correcting " "existing tests"
+                        ),
                     },
                     {
-                        "key": "test",
-                        "value": "Исправление или добавление тестов",
+                        "value": "build",
+                        "name": (
+                            "build: Changes that affect the build system or "
+                            "external dependencies (example scopes: pip, docker, npm)"
+                        ),
                     },
                     {
-                        "key": "build",
-                        "value": "Изменения, касающиеся системы сборки или зависимостей (примеры: deps, docker)",
-                    },
-                    {
-                        "key": "ci",
-                        "value": "Изменения конфига Github Actions",
+                        "value": "ci",
+                        "name": (
+                            "ci: Changes to our CI configuration files and "
+                            "scripts (example scopes: GitLabCI)"
+                        ),
                     },
                 ],
             },
-            {"type": "input", "name": "title", "message": "Commit title"},
+            {
+                "type": "input",
+                "name": "scope",
+                "message": (
+                    "What is the scope of this change? (class or file name): (press [enter] to skip)\n"
+                ),
+            },
+            {
+                "type": "input",
+                "name": "subject",
+                "message": (
+                    "Write a short and imperative summary of the code changes: (lower case and no period)\n"
+                ),
+            },
+            {
+                "type": "input",
+                "name": "body",
+                "message": (
+                    "Provide additional contextual information about the code changes: (press [enter] to skip)\n"
+                ),
+            },
+            {
+                "type": "confirm",
+                "message": "Is this a BREAKING CHANGE? Correlates with MAJOR in SemVer",
+                "name": "is_breaking_change",
+                "default": False,
+            },
+            {
+                "type": "input",
+                "name": "footer",
+                "message": (
+                    "Footer. Information about Breaking Changes and "
+                    "reference issues that this commit closes: (press [enter] to skip)\n"
+                ),
+            },
         ]
         return questions
 
     def message(self, answers: dict) -> str:
         """Generate the message with the given answers."""
-        return "{0}: {1}".format(answers.get("type"), answers.get("title"))
+        prefix = answers["prefix"]
+        scope = answers["scope"]
+        subject = answers["subject"]
+        body = answers["body"]
+        footer = answers["footer"]
+        is_breaking_change = answers["is_breaking_change"]
+
+        if scope:
+            scope = f"({scope})"
+        if body:
+            body = f"\n\n{body}"
+        if is_breaking_change:
+            footer = f"BREAKING CHANGE: {footer}"
+        if footer:
+            footer = f"\n\n{footer}"
+
+        message = f"{prefix}{scope}: {subject}{body}{footer}"
+
+        return message
+
+    def example(self) -> str:
+        return (
+            "fix: correct minor typos in code\n"
+            "\n"
+            "see the issue for details on the typos fixed\n"
+            "\n"
+            "closes issue #12"
+        )
+
+    def schema(self) -> str:
+        return (
+            "<type>(<scope>): <subject>\n"
+            "<BLANK LINE>\n"
+            "<body>\n"
+            "<BLANK LINE>\n"
+            "(BREAKING CHANGE: )<footer>"
+        )
+
+    def schema_pattern(self) -> str:
+        PATTERN = (
+            r"(build|ci|docs|feat|fix|perf|refactor|style|test|chore|revert|bump)!?"
+            r"(\(\S+\))?:(\s.*)"
+        )
+        return PATTERN
+
+    def info(self) -> str:
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        filepath = os.path.join(dir_path, "conventional_commits_info.txt")
+        with open(filepath, "r") as f:
+            content = f.read()
+        return content
+
+    def process_commit(self, commit: str) -> str:
+        pat = re.compile(self.schema_pattern())
+        m = re.match(pat, commit)
+        if m is None:
+            return ""
+        return m.group(3).strip()
 
 
 discover_this = JacobCz
